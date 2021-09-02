64ba5c5597813d6861f2fdb9cd999ff8c29ccc18
william9098 <williamjmaher88@gmail.com>
2021-09-02 14:23:53 -0500

    trying

---
+++ helo.py
@@ -0,0 +1,16 @@
+# William Maher
+# 9/02/2021
+# Comments are ignored by computer and so are empty lines
+
+# program will calculate the average of 4 tests
+#these are my variables:
+test1=90
+test2=87
+test3=89
+test4=93
+
+#declare average
+average= (test1+test2+test3+test4)/4
+#display the average
+print("The average is")
+print(average)
