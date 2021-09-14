a339de37806f892a31e097105a3e46dbaaea29f8
william9098 <williamjmaher88@gmail.com>
2021-09-13 20:43:55 -0500

    night work 9.13.21

---
+++ strings.py
@@ -0,0 +1,19 @@
+#William Maher
+#9/13/21
+
+
+#a= "Hello"
+#print(a)
+
+a = """Guranksdfjasdlkfjsdklfjsdlk fjakldfj
+Joefjaslkfjaklfsdjfklasjfklsdjfkslfjdskl"""
+
+print(a)
+
+#strings are arrays 
+
+for x in "William":
+    print(x)
+
+b= "William is amazing"
+print(len(a))
