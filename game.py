ff0ba32cad11fae24d89e2af7e19ae847094e6bb
william9098 <williamjmaher88@gmail.com>
2021-10-07 09:22:12 -0500

    10/7/21 mid of cla

---
+++ game.py
@@ -0,0 +1,6 @@
+#minumimum for game
+#1 Menu- level 1, level 2, level 3, scoreboard, exit
+#2 allow choices
+#3 play the game
+#4 keep the score- record highest score0 fucntioin to deal with score when game is over 
+#score= 3 points x word count + 5 x turns
