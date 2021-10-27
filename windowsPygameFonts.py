53593c81214ff6004d5482e1ece57012548c6776
william9098 <williamjmaher88@gmail.com>
2021-10-26 21:04:04 -0500

    10/26/21

--- windowsPygameFonts.py
+++ windowsPygameFonts.py
@@ -1,41 +1,70 @@
 #william maher
 #10/25/21 Learning Fonts and Blit
 
-import pygame, os, random, time
-
-from pygame.display import update
+import pygame as py, os, random, time
 
 
-pygame.init()
+
+
+py.init()
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 WIDTH=800
 HEIGHT=800
-window=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Setting Window")
+window=py.display.set_mode((WIDTH,HEIGHT))
+py.display.set_caption("Setting Window")
 
 #TITLE_FONT=pygame.font.SysFont(name, size, bold=False, italic=False)
-TITLE_FONT=pygame.font.SysFont('comicsans', 80)
-SubTitle=pygame.font.SysFont('comicsans', 40, italic=True)
+TITLE_FONT=py.font.SysFont('comicsans', 80)
+SubTitle=py.font.SysFont('comicsans', 40, italic=True)
+
 def display_message(message):
-    pygame.time.delay(100)
+    #py.time.delay(100)
     text= TITLE_FONT.render(message, 1, WHITE) #render prints text
    # window.blit(text, (WIDTH/2-text.get_width()/2), (HEIGHT/2-text.get_height()/2))
+    window.blit(text, (WIDTH/2-text.get_width()/2, 10))
+    py.display.update()
+    py.time.delay(100)
+    
+
+def display_message2(message):
+    py.time.delay(100)
+    text= TITLE_FONT.render(message, 2, WHITE) #render prints text
+   # window.blit(text, (WIDTH/2-text.get_width()/2), (HEIGHT/2-text.get_height()/2))
     window.blit(text, (WIDTH/2-text.get_width()/2, 70))
-    pygame.display.update()
-    pygame.time.delay(100)
+    py.display.update()
+    #py.time.delay(100)
+
+def display_message3(message):
+    #py.time.delay(100)
+    text= TITLE_FONT.render(message, 3, WHITE) 
+    window.blit(text, (WIDTH/2-text.get_width()/2, 130))
+    py.display.update()
+    #py.time.delay(100)
+
+def display_message4(message):
+    #py.time.delay(100)
+    text= TITLE_FONT.render(message, 4, WHITE) 
+    window.blit(text, (WIDTH/2-text.get_width()/2, 200))
+    py.display.update()
+    #py.time.delay(100)
 
 
 
 run=True 
 while run:
-    for eve in pygame.event.get():
-        if eve.type == pygame.QUIT: #pygame.QUIT is looking for a key pressed, while pygame.quit() is a function
+    for eve in py.event.get():
+        if eve.type == py.QUIT: #pygame.QUIT is looking for a key pressed, while pygame.quit() is a function
             run=False
-            pygame.quit()
-    window.fill((0,0,0))
+            py.quit()
+        window.fill((0,0,0))
     display_message("SETTINGS")
-    pygame.time.delay(300)
+    display_message2("Background Color")
+    display_message3("Object Color")
+    display_message4("Sound=On/Off")
+    #py.time.delay(300)
+    py.display.update()
+
 
 #print the rest of your menu for settings
 # 
@@ -53,4 +82,3 @@ while run:
 #     display_message("AHHHH")
 #     pygame.time.delay(300)
 #     window.fill(BLACK)
-pygame.quit()
